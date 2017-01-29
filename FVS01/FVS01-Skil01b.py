3de69c586dac7f76163c15f2f3515a4bc69e526b
MaggiBenni588 <magnusgu588@nemi.tskoli.is>
2017-01-29 23:13:57 +0000

    FVS01-Skil01b
    FVS01-Skil01a fyrstu skil b.

---
+++ FVS01/FVS01-Skil01b.py
@@ -0,0 +1,73 @@
+
+# coding: utf-8
+
+# ***Skilaverkefni 1 ***
+#
+# Verkefnið gildir 10% af lokaeinkunn og gilda allir liðir verkefnisins jafnt
+#
+# **\# muna að kommenta verkefnið VEL – einkunn verður lækkuð um tvo heila ef ekki er nægjanlega vel kommentað**
+#
+# Í þessu verkefni eru nokkrir liðir, settu \#comment sem afmarkar hvern lið.
+
+# ***\#Liður 1 Notandi:***
+#
+# Forritið biður um nafn notanda og skrifar síðan út:
+#
+# "**Velkomin í áfangann FOR1TÖ3AU** “ síðan *nafn notand*a og síðan ". **Þetta verður** **skemmtileg önn, ég hlakka til að læra forritun."**
+
+# ***\#Liður 2 Formatta kommutölu:***
+#
+# Forritið biður notanda að slá inn kommutölu með þrem aukastöfum og svarar síðan ”**Þú hefur valið töluna**: ” og síðan birtist talan sem slegin var inn með **einum aukastaf**.
+
+# ***\#Liður 3 - Reikniaðgerðir:***
+#
+# Forritið biður um tvær heiltölur, það á síðan að margfalda þessar tvær tölur saman og birta útkomuna á skjáinn.
+#
+# Svo á forritið að draga fyrri töluna sem sleginn var inn frá þeirri seinni og skrifa þá útkomu á skjáinn.
+
+# ***\#Liður 4- Meðalaldur:***
+#
+# Hannið forrit sem biður um aldur á þremur einstaklingum. Síðan á forritið að reikna út meðalaldur þeirra og birta hann með einum aukastaf
+
+# ***\#Liður 5 Minnsta talan:***
+#
+# \#Hérna notum við skilyrðissetningur (if-elif-else)
+#
+# Forritið spyr um þrjár heiltölur.
+#
+# Passa að ekki sé slegin inn sama tala tvisvar sinnum.
+#
+# Forritið skrifar síðan út hver talnanna er í miðjunni.
+
+# ***\#Liður 6 - Árstíðir:***
+#
+# \#Hérna notum við skilyrðissetningur (if-elif-else)
+#
+# Forritið spyr um númer mánaðar. Forritið skrifar síðan út eftirfarandi:
+#
+# Ef sleginn er inn talan 1‐3 skrifast út **”Nú er daginn tekið að lengja.”**
+# Ef sleginn er inn talan 4‐5 skrifast út **”Vorið er komið og grundirnar gróa.”**
+#
+# Ef sleginn er inn talan 6‐8 skrifast út **”Núna er sumarið komið með blóm í haga.”**
+#
+# Ef sleginn er inn talan 9‐10 skrifast út **”Núna er haustið gengið í garð.”**
+#
+# Ef sleginn er inn talan 11‐12 **”Núna styttist í jólin”**
+#
+# Ef sleginn er inn talan sem er 0 eða minni eða 13 og hærri á að skrifa **“Rangur innsláttur”.**
+
+# **\# Lidur 7:Talnabil**
+#
+# \#Hérna notum við skilyrðissetningur (if-elif-else)
+#
+# Forritið biður notanda um tölu sem er lægri en 0 eða hærri en 10.
+#
+# Ef notandi slær inn tölu frá 0 til 10 (að þeim meðtöldum) birtist "Þessi tala er ekki lægri en núll eða hærri en 10". Annars svarar forritið "Vel gert!".
+
+# ***\#Liður 8 að kveðja:***
+#
+# Að lokum birtist á skjánum:
+#
+# **Gaman að geta aðstoðað þig við þessa útreikninga –** *nafn notandans***-. **
+
+# In[ ]:
